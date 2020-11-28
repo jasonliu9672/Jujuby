1fce487d37e4b1ceb29723148f2f90deb8e1a304
Jason <jasonliu9672@gmail.com>
2020-11-28 17:01:16 +0800

    organize project structure

--- Controller/migrate_ui_build.sh
+++
@@ -1,8 +0,0 @@
-cd twitch_crawler_ui
-flutter pub get
-flutter build web
-cd ..
-rm -r src/public/*
-# cp -R /home/nslab/Desktop/controller/ui/build/web/* /home/nslab/Desktop/controller/src/public
-cp -R twitch_crawler_ui/build/web/* src/public
-
